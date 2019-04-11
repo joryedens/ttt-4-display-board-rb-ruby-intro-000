695e251f4a09f4fd5a95461216ba136c0b24595f
Jory Edens <jory.edens@gmail.com>
2019-04-11 19:57:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts " 0 | 1 | 2 "
+  puts "-----------"
+  puts " 3 | 4 | 5 "
+  puts "-----------"
+  puts " 6 | 7 | 8 "
+end 
